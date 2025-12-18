9b92f06fb2e0df73133593dae2c9f72943cbd6e9
thanh <thanh@riselabs.xyz>
2025-12-18 14:21:21 +0700

    chore: update gravity exchange fee

--- migrations/07_add_gravity_exchange.sql
+++ migrations/07_add_gravity_exchange.sql
@@ -3,6 +3,6 @@
 
 -- UP: Insert Gravity exchange if it doesn't already exist
 INSERT INTO exchanges (name, maker_fee, taker_fee)
-VALUES ('gravity', 0.0001, 0.0005)
+VALUES ('gravity', -0.00003, 0.00024) -- -0.003% / 0.024% (LV9)
 ON CONFLICT (name) DO NOTHING;
 
